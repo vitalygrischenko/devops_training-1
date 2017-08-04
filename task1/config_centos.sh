57f6edb2cd3ee6b286fce9b9e160a043a1b9d9c2
Maksim Danilchenko <ms.danilchenko@gmail.com>
2017-08-04 22:27:00 +0300

    task1

--- task1/config_centos.sh
+++ task1/config_centos.sh
@@ -2,4 +2,4 @@
 sudo yum update -y
 sudo yum install -y git
 sudo ssh-agent bash -c 'ssh-add /shared/id_rsa'
-git clone -b mdanilchenko https://github.com/mdanilchenko/devops_training.git
+git clone -b task1 https://github.com/mdanilchenko/devops_training.git
