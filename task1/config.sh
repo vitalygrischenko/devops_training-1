588b1e901a4318d83dc1d33a2cd213bea49d16f9
mdanilchenko <ms.danilchenko@gmail.com>
2017-08-07 21:40:02 +0300

    Update config.sh

--- task1/config.sh
+++ task1/config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install -y git
-git clone -b mdanilchenko https://github.com/mdanilchenko/devops_training.git
+git clone -b task1 https://github.com/mdanilchenko/devops_training.git
