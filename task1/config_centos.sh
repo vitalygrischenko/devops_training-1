33df8e20b457c5671da0341531f35572750f58dd
Maksim Danilchenko <ms.danilchenko@gmail.com>
2017-08-08 10:45:27 +0300

    sudo removed

--- task1/config_centos.sh
+++ task1/config_centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y git
-sudo ssh-agent bash -c 'ssh-add /shared/id_rsa'
+yum update -y
+yum install -y git
+ssh-agent bash -c 'ssh-add /shared/id_rsa'
 git clone -b task1 https://github.com/mdanilchenko/devops_training.git
 cat devops_training/README.md
